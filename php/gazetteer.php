3165084c17b4f393669ca88e4ef0bc2d82ab6e62
Aemack <adam_mcgrane@msn.com>
2020-08-12 22:06:25 +0100

    Update gazetteer.php

--- php/gazetteer.php
+++ php/gazetteer.php
@@ -46,7 +46,7 @@ function getAllData($name){
     
     $r = json_decode($json_result, true);
 
-    $result["result"]["currency"]["exchRate"]=$r["rates"][$result["currency"]["iso"]];
+    $result["result"]["currency"]["exchRate"]=$r["rates"];//[$result["currency"]["iso"]];
 
 
     
