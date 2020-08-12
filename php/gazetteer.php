fb1c374226930ac24dd3fe17e4ee5f46c7adb1df
Aemack <adam_mcgrane@msn.com>
2020-08-12 22:34:01 +0100

    Update gazetteer.php

--- php/gazetteer.php
+++ php/gazetteer.php
@@ -86,12 +86,9 @@ function getCountries(){
     
     
     for ($i=0; $i < count($r) ;$i++){
-       /* if (strpos($r[$i]["name"], 'Korea') !== false) {
-            $r[$i]["name"]="Korea (South)";
+        if (strpos($r[$i]["name"], 'Korea') !== false) {
+            $r[$i]["name"]="Korea";
         }
-        if (strpos($r[$i]["name"], 'Palestine') !== false) {
-            $r[$i]["name"]="Palestine";
-        }*/
         $result[$i] = $r[$i]["name"];
     }
     
