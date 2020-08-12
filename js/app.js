c76e123d43a01d6b4e71b5de9adff8b476d74eeb
Aemack <adam_mcgrane@msn.com>
2020-08-12 22:28:17 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,11 +9,6 @@ function clearOutput(){
     })
 }
 
-
-function logData(obj){
-    console.log(obj)
-}
-
 function getCountryData(lat,lng){
         document.querySelector(".modal-title").innerText = $("#countryQuery").val();
         $("#loadingImageMod").hide();
@@ -111,125 +106,7 @@ function loadMap(lat,long){
 function newMap(lat, lng){
     mymap.panTo(new L.LatLng(lat, lng));
 }
-/*
-//Gets Country/Lat/Long/ISOCodes/CurrencyName/CurrencyISOCode/
-function getCountryData(lat,lng){
-    document.querySelector(".modal-title").innerText = $("#countryQuery").val();
-    $("#loadingImageMod").hide();
-    $("#mapid").hide()
-    $("#loadingImage").show()
-    
-    clearOutput()
-    if (!lat && !lng){
-        country = $("#countryQuery").val().split(" ").join("%20");
-        if (country === "Palestine"){
-            obj = {
-                ISoa2:"PS",
-                ISOa3:"PSE",
-                capital:"Jerusalem",
-                country:"Palestinian Territory",
-                geometry:{lat:31.2752047,lng:34.2558269},
-                currency:{
-                    iso:"EGP",
-                    name:"Egyptian pound"
-                }
 
-            }
-            restCountriesData(obj)
-        }else{
-        jQuery.ajax({
-            type: "POST",
-            url: 'php/gazetteer.php',
-            dataType: 'json',
-            data: {functionname: 'getCountryData', arguments: [country]},
-            success: restCountriesData,
-            error:function(){
-                console.log("Could not locate")
-                jQuery.ajax({
-                    type: "POST",
-                    url: 'php/gazetteer.php',
-                    dataType: 'json',
-                    data: {functionname: 'getAltData', arguments: [country]},
-                    success: restCountriesData,
-                    error: ()=>{console.log("Error: "+country)}
-                })
-            }
-        })}
-    } else {
-        latlng = lat+","+lng
-        jQuery.ajax({
-            type: "POST",
-            url: 'php/gazetteer.php',
-            dataType: 'json',
-            data: {functionname: 'getCountryData', arguments: [latlng]},
-            success: restCountriesData,
-            error:function(){
-                
-                console.log("ERRUR: CUD NUT FIND CUNTRY")
-            }
-        })
-    }
-
-} 
-
-//Gets population/capital/flag
-function restCountriesData(obj){
-    countryData = obj;
-    console.log(obj)
-    jQuery.ajax({
-        type: "POST",
-        url: 'php/gazetteer.php',
-        dataType: 'json',
-        data: {functionname: 'restCountriesData', arguments: [obj.ISOa3]},
-        success: getExchange,
-        error:function(){
-            
-            console.log("Error: Couldn't get pop/capi/flag")
-        }
-    })
-
-}
-
-//Gets Exchange rate
-function getExchange(obj){
-    countryData.capital = obj.capital;
-    countryData.flag = obj.flag;
-    countryData.population = obj.population;
-
-
-    jQuery.ajax({
-        type: "POST",
-        url: 'php/gazetteer.php',
-        dataType: 'json',
-        data: {functionname: 'getExchange', arguments: [countryData.currency.iso]},
-        success: getWeather,
-        error:function(){
-            
-            console.log("Error: Couldn't get Exch")
-            getWeather({exchRate:"Could Not Retrieve Exchange Rate"})
-        }
-    })
-      
-    
-}
-
-//Get 5 day weather forecast
-function getWeather(obj){
-    countryData.currency.exchRate = obj.exchRate;
-
-    jQuery.ajax({
-        type: "POST",
-        url: 'php/gazetteer.php',
-        dataType: 'json',
-        data: {functionname: 'getWeather', arguments: [countryData.geometry.lat,countryData.geometry.lng]},
-        success: logData,
-        error:function(){
-            
-            console.log("ERRUR: CUD NUT FIND CUNTRY")
-        }
-    })
-}
-*/
 function logData(obj){
     countryData = obj.result;
     console.log(countryData)
